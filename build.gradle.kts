cd70c647e9947784f12adbf608a713083e4382c5
boot2big <mmangamer14yt@gmail.com>
2023-04-12 12:51:16 -0400

    Magic builder (it is cool)

---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+import java.nio.file.Paths
+import kotlin.io.path.moveTo
+import kotlin.io.path.ExperimentalPathApi
+
+
+plugins {
+    java
+    kotlin("jvm") version "1.7.20"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+subprojects {
+    apply(plugin = "java")
+}
+
+var modVersion: String = project.property("global_version").toString()
+
+//var modVersion: String = providers.gradleProperty("global_version")
+
+var mcCore = project(":mccore")
+var mcInterfaceForge1122 = project(":mcinterfaceforge1122")
+var mcInterfaceForge1165 = project(":mcinterfaceforge1165")
+
+tasks.register("buildCore") {
+    dependsOn(mcCore.tasks.build)
+    doLast {
+        moveToOut(mcCore, "core")
+    }
+}
+
+tasks.register("buildForge1122") {
+    doFirst { preBuild() }
+    doLast {
+        moveToOut(mcInterfaceForge1122, "1.12.2")
+    }
+    dependsOn(mcInterfaceForge1122.tasks.build)
+}
+
+tasks.register("buildForge1165") {
+    doFirst { preBuild() }
+    doLast {
+        moveToOut(mcInterfaceForge1165, "1.16.5")
+    }
+    dependsOn(mcInterfaceForge1165.tasks.build)
+}
+
+tasks.register("buildForgeAll") {
+    dependsOn(tasks.getByName("buildForge1122"))
+    dependsOn(tasks.getByName("buildForge1165"))
+}
+
+@OptIn(ExperimentalPathApi::class)
+fun moveToOut(subProject: Project, versionStr: String) {
+    mkdir("out/$versionStr")
+    val jarName = "Trin Civil Pack-${subProject.version}.jar"
+    Paths.get("${subProject.projectDir.canonicalPath}/build/libs/$jarName")
+        .moveTo(Paths.get("${project.projectDir.canonicalPath}/out/$jarName"), true)
+}
+
+fun preBuild() {
+    // Could probably be better somehow, but I'm not sure how
+    project.projectDir.canonicalFile.walk()
+        .filter { it.name == "gradle.properties" || it.name == "mcmod.info" || it.name == "InterfaceLoader.java" }
+        .forEach { it.writeText(it.readText()
+            .replace(Regex("mod_version=(.+)"), "mod_version=$modVersion")
+            .replace(Regex("\"version\": \"[^\"]*\""), "\"version\": \"$modVersion\"")
+            .replace(Regex("MODVER = \"[^\"]*\";"), "MODVER = \"$modVersion\";")) }
+}
---
+++ mcinterfaceforge1165/src/main/java/iv_tcp_v4/ForgePackLoader.java
@@ -0,0 +1,15 @@
+package iv_tcp_v4;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Pack loader class.  The compiler script will configure this for your pack.
+ * This is here to make Forge shut up and load packs as mods, packs don't actually do anything so there's no code here.
+ *
+ * @author don_bruce
+ */
+@Mod("iv_tcp_v4")
+public class ForgePackLoader {
+
+    public ForgePackLoader() {}
+}
